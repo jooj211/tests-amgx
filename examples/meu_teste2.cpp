b33992428bf14adc8da3f213220f6e34494f3685
Lucas Santana <lucasg4x@gmail.com>
2023-05-04 23:46:42 -0300

    funfando na imune

---
+++ examples/meu_teste2.cpp
@@ -0,0 +1,455 @@
+#ifdef AMGX_DYNAMIC_LOADING
+#include "amgx_capi.h"
+#else
+#include "amgx_c.h"
+#endif
+
+#include <stdio.h>
+#include <amgx_c.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+void arquivoErroRelativo(double xi, double ve, double va, double *somaER)
+{
+    ofstream erroRelativo;
+    erroRelativo.open("erroRelativo.csv", ios_base::app);
+
+    double ER = va - ve;
+
+    *somaER = *somaER + (ER * ER);
+
+    erroRelativo << xi << "," << ve << "," << va << "," << ER << "," << ER * ER << endl;
+
+    erroRelativo.close();
+}
+
+void calcular(const char **argv)
+{
+
+    AMGX_initialize();
+    // AMGX_initialize_plugins();//The AMGX_initialize_plugins API call is deprecated and can be safely removed.
+
+    // All of the objects are initialized using a default Resources.
+    AMGX_matrix_handle matrix;
+    AMGX_vector_handle rhs;
+    AMGX_vector_handle soln;
+    AMGX_resources_handle rsrc;
+    AMGX_solver_handle solver;
+    AMGX_config_handle config;
+
+    // arquivo de configuração passado como parametro: ../src/configs/FGMRES_AGGREGATION_JACOBI.json
+    AMGX_config_create_from_file(&config, argv[4]);
+    AMGX_resources_create_simple(&rsrc, config);
+
+    AMGX_matrix_create(&matrix, rsrc, AMGX_mode_dDDI);
+    AMGX_vector_create(&rhs, rsrc, AMGX_mode_dDDI);
+    AMGX_vector_create(&soln, rsrc, AMGX_mode_dDDI);
+
+    double data[1000000];
+
+    /*
+        1 d -> the first letter h or d specifies whether the matrix data (and subsequent linear solver algorithms) will run on the host or device
+        2 D(or F) -> specifies the precision (double or float) of the Matrix data
+        3 D The third D or F specifies the precision (double or float) of any Vector (including right-hand side or unknown vectors)
+        4 I The last I specifies that 32-bit int types are used for all indices
+    */
+
+    AMGX_solver_create(&solver, rsrc, AMGX_mode_dDDI, config);
+
+    // Next, data is uploaded from the application (or set, in the case of the solution vector which is initialized to all zeroes).
+    //  If these are not specified than rhs=[1,...,1]^T and (initial guess) sol=[0,...,0]^T.
+    AMGX_read_system(matrix, rhs, soln, argv[2]);
+
+    AMGX_write_system(matrix, rhs, soln, "./output.system.mtx");
+    AMGX_solver_setup(solver, matrix);
+
+    AMGX_solver_solve_with_0_initial_guess(solver, rhs, soln);
+
+    AMGX_vector_download(soln, data);
+
+    int sol_size, sol_bsize;
+    AMGX_vector_get_size(soln, &sol_size, &sol_bsize);
+
+    cout << "Solução: " << sol_size << endl;
+    cout << "Solução b_size: " << sol_bsize << endl;
+
+    // descobrir o stepSize
+    double a = -1, b = 1; // intervalo
+    double stepSize = (b - a) / (sol_size + 1);
+
+    cout << "STEP SIZE: " << stepSize << endl;
+
+    double step = a;
+    double valorExato;
+    double ER;
+
+    cout << "STEP"
+         << "\t"
+         << "V. EXATO"
+         << "\t"
+         << "V. APROX"
+         << "\t" << endl;
+    ofstream erroRelativo, normaL2;
+
+    erroRelativo.open("erroRelativo.csv");
+    erroRelativo << "x,valor exato,valor aproximado, ER,ER²" << endl;
+    erroRelativo << "-1,2,2,0,0" << endl;
+    erroRelativo.close();
+
+    double somaER = 0; // soma dos quadrado do erro relativo
+    for (int i = 0; i < sol_size; ++i)
+    {
+        step += stepSize;
+        valorExato = (1 + step * step); // u = 1+x²
+        ER = data[i] - valorExato;
+
+        arquivoErroRelativo(step, valorExato, data[i], &somaER);
+        cout << step << "\t\t" << valorExato << "\t\t" << data[i] << "\t\t" << ER << endl;
+    }
+
+    somaER = somaER * stepSize; // pela fórmula do slide tenho que multiplicar pelo h
+    cout << "SOMA ER²: " << somaER << endl;
+    cout << "RAIZ ER²: " << sqrt(somaER) << endl;
+
+    erroRelativo.open("erroRelativo.csv", ios_base::app);
+    erroRelativo << "1,2,2,0,0" << endl;
+    erroRelativo.close();
+
+    // plotar o grafico do logaritmo da norma l2
+    normaL2.open("normaL2.csv", ios_base::app);
+    normaL2 << "x,y" << endl;
+    normaL2 << sol_size << "," << sqrt(somaER) << endl;
+    normaL2.close();
+
+    AMGX_solver_destroy(solver);
+    AMGX_vector_destroy(soln);
+    AMGX_vector_destroy(rhs);
+    AMGX_matrix_destroy(matrix);
+    AMGX_resources_destroy(rsrc);
+
+    AMGX_SAFE_CALL(AMGX_config_destroy(config));
+    AMGX_SAFE_CALL(AMGX_finalize());
+}
+
+void gerarMatrizS(double stepSize)
+{
+
+    double a = -1, b = 1; // intervalo
+
+    double alfa = 2.0;
+    double beta = 2.0;
+
+    int numeroPontos = ((b - a) / stepSize) + 1;
+    cout << "numeroPontos: " << numeroPontos << endl;
+
+    /*
+        numeroPontos = 9 são 7 valores + 2 valores de contorno
+        na resolução do sistema não uso o contorno então faço -2
+    */
+
+    int n = numeroPontos - 2;
+
+    // double stepSize = (b - a) / (numeroPontos - 1); // valor de cada incremento, 9 pontos mas 8 intervalos
+    cout << "n: " << n << endl;
+    cout << "STEP SIZE: " << stepSize << endl;
+
+    double h = stepSize * stepSize;
+    cout << "h: " << h << endl;
+
+    int nnz = 3 * n - 2; // elementos não-nulos - n+(n-1)+(n-1) = 3n-2
+    cout << "NNZ: " << nnz << endl;
+    int countI = 1, countF = 3;
+
+    ofstream arquivoMtx;
+
+    arquivoMtx.open("../examples/matrix3.mtx");
+    arquivoMtx <<fixed<< scientific << setprecision(15);
+    arquivoMtx << "%%MatrixMarket matrix coordinate real general symmetric" << endl;
+    arquivoMtx << "%%AMGX 1 1 sorted rhs" << endl;
+    arquivoMtx << n << " " << n << " " << nnz << endl;
+    arquivoMtx << 1 << " " << 1 << " " << 2.0 << endl;
+    arquivoMtx << 1 << " " << 2 << " " << -1.0 << endl;
+
+    for (int i = 2; i <= n - 1; i++)
+    {
+        for (int j = countI; j <= countF; j++)
+        {
+            if (i == j)
+            {
+                arquivoMtx << i << " " << j << " " << 2.0 << endl;
+            }
+            else
+            {
+                arquivoMtx << i << " " << j << " " << -1.0 << endl;
+            }
+        }
+        countI++;
+        countF++;
+    }
+
+    arquivoMtx << n << " " << n - 1 << " " << -1.0 << endl;
+    arquivoMtx << n << " " << n << " " << 2.0 << endl;
+
+    double vd = -h * 2.0 + alfa;
+    cout << "RHS: " << vd << endl;
+
+    arquivoMtx << vd << endl;
+    for (int i = 0; i < n - 2; i++)
+    {
+        arquivoMtx << -2.0 * h << endl;
+    }
+    arquivoMtx << vd;
+
+    arquivoMtx.close();
+}
+
+void gerarMatrizN(double numeroPontos)
+{
+
+    double a = -1, b = 1; // intervalo
+
+    double alfa = 2.0;
+    double beta = 2.0;
+
+    cout << "numeroPontos: " << numeroPontos << endl;
+
+    /*
+        numeroPontos = 9 são 7 valores + 2 valores de contorno
+        na resolução do sistema não uso o contorno então faço -2
+    */
+
+    int n = numeroPontos - 2;
+
+    double stepSize = (b - a) / (numeroPontos - 1); // valor de cada incremento, 9 pontos mas 8 intervalos
+    cout << "n: " << n << endl;
+    cout << "STEP SIZE: " << stepSize << endl;
+
+    double h = stepSize * stepSize;
+    cout << "h: " << h << endl;
+
+    int nnz = 3 * n - 2; // elementos não-nulos - n+(n-1)+(n-1) = 3n-2
+    cout << "NNZ: " << nnz << endl;
+    int countI = 1, countF = 3;
+
+    ofstream arquivoMtx;
+
+    arquivoMtx.open("../examples/matrix3.mtx");
+    // arquivoMtx << scientific << setprecision(15);
+    arquivoMtx << "%%MatrixMarket matrix coordinate real general symmetric" << endl;
+    arquivoMtx << "%%AMGX 1 1 sorted rhs" << endl;
+    arquivoMtx << n << " " << n << " " << nnz << endl;
+    arquivoMtx << 1 << " " << 1 << " " << 2.0 << endl;
+    arquivoMtx << 1 << " " << 2 << " " << -1.0 << endl;
+
+    for (int i = 2; i <= n - 1; i++)
+    {
+        for (int j = countI; j <= countF; j++)
+        {
+            if (i == j)
+            {
+                arquivoMtx << i << " " << j << " " << 2.0 << endl;
+            }
+            else
+            {
+                arquivoMtx << i << " " << j << " " << -1.0 << endl;
+            }
+        }
+        countI++;
+        countF++;
+    }
+
+    arquivoMtx << n << " " << n - 1 << " " << -1.0 << endl;
+    arquivoMtx << n << " " << n << " " << 2.0 << endl;
+
+    double vd = -h * 2.0 + alfa;
+    cout << "RHS: " << vd << endl;
+
+    arquivoMtx << vd << endl;
+    for (int i = 0; i < n - 2; i++)
+    {
+        arquivoMtx << -2.0 * h << endl;
+    }
+    arquivoMtx << vd;
+
+    arquivoMtx.close();
+}
+
+int main(int argc, const char **argv)
+{
+    /*
+        para rodar, de dentro da pasta build: make -j16 all && examples/meu_teste -m ../examples/matrix.mtx -c ../src/configs/FGMRES_AGGREGATION.json
+    */
+
+    /*
+     g++ gerarMatriz.cpp  -o gerarMtx && ./gerarMtx 9 && rm gerarMtx && make -j16 all && examples/meu_teste2 -m ../examples/matrix3.mtx -c ../src/configs/FGMRES_AGGREGATION.json
+     g++ ../examples/gerarMatriz.cpp  -o gerarMtx && ./gerarMtx 1000 && rm gerarMtx && make -j16 all && examples/meu_teste2 -m ../examples/matrix3.mtx -c ../src/configs/FGMRES_AGGREGATION.json
+     make -j16 all && examples/meu_teste2 -m ../examples/matrix3.mtx -c ../src/configs/FGMRES_AGGREGATION.json -n 9
+     
+     make -j16 all && examples/meu_teste2 -m ../examples/matrix3.mtx -c ../src/configs/FGMRES_AGGREGATION.json -s 0.025
+     make -j16 all && examples/meu_teste2 -m ../examples/matrix3.mtx -c ../src/configs/FGMRES_AGGREGATION.json -n 9
+     */
+
+    /*
+        TODO:
+            0.0000002
+            Rodar na imune aumentando o step (0.25, 0.025,...) e anotar tempo,numero iterações, numero de pontos,step erros, L2 e outras info relevantes
+            Plotar grafico L2
+    */
+    /*
+        matriz formato CSR
+        pegar matriz já resolvida anteriormente e comparar resultados, primeiro sem RHS (tudo com 1)
+        depois com RHS ()
+
+        https://www.bu.edu/pasi/files/2011/07/Lecture7.pdf
+        https://github.com/cusplibrary/cusplibrary/blob/develop/examples/MatrixFormats/coo.cu
+        https://stackoverflow.com/questions/14097004/convert-a-matrix-a-in-a-sparse-formats-csr-coo-etc
+
+    */
+
+    cout << "Arquivo matrix: " << argv[2] << endl;
+    cout << "Config: " << argv[4] << endl;
+    int numeroPontos;
+    double stepSize;
+    istringstream ss(argv[6]);
+    if (std::string(argv[5]) == "-n")
+    {
+        ss >> numeroPontos;
+        cout << "Número Pontos: " << numeroPontos << endl;
+        gerarMatrizN(numeroPontos);
+    }
+    else if (std::string(argv[5]) == "-s")
+    {
+        ss >> stepSize;
+        cout << "Step Size: " << stepSize << endl;
+        gerarMatrizS(stepSize);
+    }
+
+    
+
+    // for (int i = 10; i > 0; i++)
+    // {
+    //     gerarMatriz(stepSize);
+    //     calcular(argv);
+    //     stS = stS / 2.0;
+    // }
+
+    AMGX_initialize();
+    // AMGX_initialize_plugins();//The AMGX_initialize_plugins API call is deprecated and can be safely removed.
+
+    // All of the objects are initialized using a default Resources.
+    AMGX_matrix_handle matrix;
+    AMGX_vector_handle rhs;
+    AMGX_vector_handle soln;
+    AMGX_resources_handle rsrc;
+    AMGX_solver_handle solver;
+    AMGX_config_handle config;
+
+    // arquivo de configuração passado como parametro: ../src/configs/FGMRES_AGGREGATION_JACOBI.json
+    AMGX_config_create_from_file(&config, argv[4]);
+    AMGX_resources_create_simple(&rsrc, config);
+
+    AMGX_matrix_create(&matrix, rsrc, AMGX_mode_dDDI);
+    AMGX_vector_create(&rhs, rsrc, AMGX_mode_dDDI);
+    AMGX_vector_create(&soln, rsrc, AMGX_mode_dDDI);
+
+    double *data = new double[100000000];
+
+    /*
+        1 d -> the first letter h or d specifies whether the matrix data (and subsequent linear solver algorithms) will run on the host or device
+        2 D(or F) -> specifies the precision (double or float) of the Matrix data
+        3 D The third D or F specifies the precision (double or float) of any Vector (including right-hand side or unknown vectors)
+        4 I The last I specifies that 32-bit int types are used for all indices
+    */
+    AMGX_solver_create(&solver, rsrc, AMGX_mode_dDDI, config);
+
+    // Next, data is uploaded from the application (or set, in the case of the solution vector which is initialized to all zeroes).
+    //  If these are not specified than rhs=[1,...,1]^T and (initial guess) sol=[0,...,0]^T.
+    AMGX_read_system(matrix, rhs, soln, argv[2]);
+
+    //AMGX_write_system(matrix, rhs, soln, "./output.system.mtx");
+    AMGX_solver_setup(solver, matrix);
+
+    AMGX_solver_solve_with_0_initial_guess(solver, rhs, soln);
+
+    AMGX_vector_download(soln, data);
+
+    int sol_size, sol_bsize;
+    AMGX_vector_get_size(soln, &sol_size, &sol_bsize);
+
+    
+
+    for (int i = 0; i < sol_size; ++i)
+    {
+        printf("%f \n",data[i]);
+    }
+
+    cout << "Solução: " << sol_size << endl;
+    cout << "Solução b_size: " << sol_bsize << endl;
+
+    /*
+    // descobrir o stepSize
+    double a = -1, b = 1; // intervalo
+    //double stepSize = (b - a) / (sol_size + 1);
+
+    cout << "STEP SIZE: " << stepSize << endl;
+
+    double step = a;
+    double valorExato;
+    double ER;
+
+    cout << "STEP"
+         << "\t"
+         << "V. EXATO"
+         << "\t"
+         << "V. APROX"
+         << "\t" << endl;
+    ofstream erroRelativo, normaL2;
+
+    erroRelativo.open("erroRelativo.csv");
+    erroRelativo << "x,valor exato,valor aproximado, ER,ER²" << endl;
+    erroRelativo << "-1,2,2,0,0" << endl;
+    erroRelativo.close();
+
+    double somaER = 0; // soma dos quadrado do erro relativo
+    for (int i = 0; i < sol_size; ++i)
+    {
+        step += stepSize;
+        valorExato = (1 + step * step); // u = 1+x²
+        ER = data[i] - valorExato;
+
+        arquivoErroRelativo(step, valorExato, data[i], &somaER);
+        cout << step << "\t\t" << valorExato << "\t\t" << data[i] << "\t\t" << ER << endl;
+    }
+
+    somaER = somaER * stepSize; // pela fórmula do slide tenho que multiplicar pelo h
+    cout << "SOMA ER²: " << somaER << endl;
+    cout << "RAIZ ER²: " << sqrt(somaER) << endl;
+
+    erroRelativo.open("erroRelativo.csv", ios_base::app);
+    erroRelativo << "1,2,2,0,0" << endl;
+    erroRelativo.close();
+
+    // plotar o grafico do logaritmo da norma l2
+    normaL2.open("normaL2.csv", ios_base::app);
+    normaL2 << sol_size << "," << sqrt(somaER) << endl;
+    normaL2.close();
+    */
+
+    AMGX_solver_destroy(solver);
+    AMGX_vector_destroy(soln);
+    AMGX_vector_destroy(rhs);
+    AMGX_matrix_destroy(matrix);
+    AMGX_resources_destroy(rsrc);
+
+    AMGX_SAFE_CALL(AMGX_config_destroy(config));
+    AMGX_SAFE_CALL(AMGX_finalize());
+
+    // AMGX_finalize_plugins();
+    // AMGX_finalize();
+    return 0;
+}
---
+++ include/thrust_wrapper.h
@@ -0,0 +1,138 @@
+#pragma once 
+
+#include <thrust/scan.h>
+#include <thrust/reduce.h>
+#include <thrust/sort.h>
+#include <thrust/gather.h>
+#include <thrust/count.h>
+#include <thrust/transform.h>
+#include <thrust/iterator/iterator_traits.h>
+#include <vector_thrust_allocator.h>
+#include <thrust/transform_reduce.h>
+#include <thrust/adjacent_difference.h>
+
+namespace thrust_wrapper
+{
+  template<typename InputIterator, typename OutputIterator>
+    inline void exclusive_scan(InputIterator first, InputIterator last, OutputIterator result)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      amgx::thrust::exclusive_scan(amgx::thrust::cuda::par_nosync(alloc), first, last, result);
+    }
+
+  template<typename InputIterator, typename OutputIterator, typename T>
+    inline void exclusive_scan(InputIterator first, InputIterator last, OutputIterator result, T init)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      amgx::thrust::exclusive_scan(amgx::thrust::cuda::par_nosync(alloc), first, last, result, init);
+    }
+
+  template<typename InputIterator, typename OutputIterator>
+    inline void inclusive_scan(InputIterator first, InputIterator last, OutputIterator result)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      amgx::thrust::inclusive_scan(amgx::thrust::cuda::par_nosync(alloc), first, last, result);
+    }
+
+  template<typename InputIterator, typename OutputIterator, typename AssociativeOperator>
+    inline void inclusive_scan(InputIterator first, InputIterator last, OutputIterator result, AssociativeOperator binary_op)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      amgx::thrust::inclusive_scan(amgx::thrust::cuda::par_nosync(alloc), first, last, result, binary_op);
+    }
+
+  template<typename RandomAccessIterator>
+    inline void sort(RandomAccessIterator first, RandomAccessIterator last)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<RandomAccessIterator>::value_type> alloc;
+      amgx::thrust::sort(amgx::thrust::cuda::par_nosync(alloc), first, last);
+    }
+
+  template<typename RandomAccessIterator1, typename RandomAccessIterator2>
+    inline void sort_by_key(RandomAccessIterator1 keys_first, RandomAccessIterator1 keys_last, RandomAccessIterator2 values_first)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<RandomAccessIterator1>::value_type> alloc;
+      amgx::thrust::sort_by_key(amgx::thrust::cuda::par_nosync(alloc), keys_first, keys_last, values_first);
+    }
+
+  template<typename RandomAccessIterator1, typename RandomAccessIterator2>
+    inline void stable_sort_by_key(RandomAccessIterator1 keys_first, RandomAccessIterator1 keys_last, RandomAccessIterator2 values_first)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<RandomAccessIterator1>::value_type> alloc;
+      amgx::thrust::stable_sort_by_key(amgx::thrust::cuda::par_nosync(alloc), keys_first, keys_last, values_first);
+    }
+
+  template<typename InputIterator>
+    inline typename amgx::thrust::iterator_traits<InputIterator>::value_type reduce(InputIterator first, InputIterator last)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      return amgx::thrust::reduce(amgx::thrust::cuda::par_nosync(alloc), first, last);
+    }
+
+  template<typename InputIterator, typename T, typename BinaryFunction>
+    inline T reduce(InputIterator first, InputIterator last, T init, BinaryFunction binary_op)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      return amgx::thrust::reduce(amgx::thrust::cuda::par_nosync(alloc), first, last, init, binary_op);
+    }
+
+  template<typename InputIterator, typename OutputIterator, typename UnaryFunction>
+    inline OutputIterator transform(InputIterator first, InputIterator last, OutputIterator result, UnaryFunction op)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      return amgx::thrust::transform(amgx::thrust::cuda::par_nosync(alloc), first, last, result, op);
+    }
+
+  template<typename InputIterator, typename OutputIterator, typename RandomAccessIterator>
+    inline OutputIterator gather(InputIterator map_first, InputIterator map_last, RandomAccessIterator input_first, OutputIterator result)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      return amgx::thrust::gather(amgx::thrust::cuda::par_nosync(alloc), map_first, map_last, input_first, result);
+    }
+
+  template<typename InputIterator , typename UnaryFunction , typename OutputType , typename BinaryFunction >
+    inline OutputType transform_reduce(InputIterator first, InputIterator last, UnaryFunction unary_op, OutputType init, BinaryFunction binary_op)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      return amgx::thrust::transform_reduce(amgx::thrust::cuda::par_nosync(alloc), first, last, unary_op, init, binary_op);
+    }
+
+  template<typename InputIterator, typename UnaryFunction>
+    inline InputIterator for_each(InputIterator first, InputIterator last, UnaryFunction f)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      return amgx::thrust::for_each(amgx::thrust::cuda::par_nosync(alloc), first, last, f);
+    }
+
+  template<typename InputIterator , typename OutputIterator >
+    inline OutputIterator copy(InputIterator first, InputIterator last, OutputIterator result, cudaStream_t stream = 0, bool sync_default = false)
+    {
+      if(sync_default) {
+        cudaStreamSynchronize(0);
+      }
+      OutputIterator res = amgx::thrust::copy(amgx::thrust::cuda::par_nosync.on(stream), first, last, result);
+      cudaStreamSynchronize(stream);
+      return res;
+    }
+
+  template<typename InputIterator, typename EqualityComparable>
+    inline typename amgx::thrust::iterator_traits<InputIterator>::difference_type count(InputIterator first, InputIterator last, const EqualityComparable& value) 
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      return amgx::thrust::count(amgx::thrust::cuda::par_nosync(alloc), first, last, value);
+    }
+
+  template<typename InputIterator, typename Predicate >
+    inline typename amgx::thrust::iterator_traits<InputIterator>::difference_type count_if(InputIterator first, InputIterator last, Predicate pred)
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      return amgx::thrust::count_if(amgx::thrust::cuda::par_nosync(alloc), first, last, pred);
+    }
+
+  template<typename InputIterator , typename OutputIterator >
+    inline OutputIterator adjacent_difference(InputIterator first, InputIterator last, OutputIterator result)	
+    {
+      amgx::thrust_amgx_allocator<typename amgx::thrust::iterator_traits<InputIterator>::value_type> alloc;
+      return amgx::thrust::adjacent_difference(amgx::thrust::cuda::par_nosync(alloc), first, last, result);
+    }
+}
